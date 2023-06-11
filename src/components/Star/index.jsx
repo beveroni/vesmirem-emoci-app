b3d22ed520850f41547156c95a4cc76b41ac9d35
Eliška Korencová <eli.korenc@gmail.com>
2023-06-11 09:50:23 +0200

    Pokus o komponenty hvězda

---
+++ src/components/Star/index.jsx
@@ -0,0 +1,24 @@
+// pokus o vykreslení hvězdy
+
+// import React from 'react';
+// import { useState } from 'react';
+// import { Game } from '../Game';
+
+// export const Star = () => {
+//   const [showStarComponent, setShowStarComponent] = useState(false);
+
+//   const handleButtonClick = () => {
+//     setShowStarComponent(true);
+//   };
+
+//   return (
+//     <div>
+//       {showStarComponent ? <Star /> : <Game />}
+//       <button onClick={handleButtonClick}>Přepnout</button>
+//     </div>
+//   );
+// };
+
+// const showStarComponent = () => {
+//   return <div>Hvězdička</div>;
+// };
