126a18ecaef5f08e259c03ef69845690b5ab2fb1
Veronika Bendova <svechovaver@gmail.com>
2023-06-13 13:07:08 +0200

    Nastylování Error Page

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import './style.css';
 
 export const ErrorPage = () => {
   return (
-    <div className="error-page">
-      <h2 className="erroPageHeading">404: Tady nic není</h2>
-      <p className="erroPageParam">Asi jste se spletli</p>
+    <div className="error__page">
+      <div className="errorpage__container">
+        <h2 className="erropage__heading">
+          <strong className='errorpage_strong'>404:</strong> Ocitl jsi se v temných hlubinách vesmíru
+        </h2>
+        <p className="erropage__param">
+          "Vypadá to, že se trošku ztrácíme ve vesmíru! Počkejme chvilku, ať nám
+          naše hvězdičky najdou správnou cestu zpět do hry. Vesmír je plný
+          dobrodružství, takže se brzy vrátíme zpátky k objevování!"
+        </p>
+      </div>
     </div>
   );
 };
