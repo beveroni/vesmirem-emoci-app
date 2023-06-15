5d14c1f952bc0479c3ef580a65fb062e8d91f264
Veronika Bendova <svechovaver@gmail.com>
2023-06-15 12:05:21 +0200

    Změna pozadí PlanetsPage

---
+++ src/components/AstronautHome/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+import { Link } from 'react-router-dom';
+
+export const AstronautHome = () => {
+  return (
+    <Link to="/">
+      <img
+        src="/img/astronaut.svg"
+        alt="obrázek kosmonauta odkazující na domovskou stránku"
+        className="astronaut_winer"
+      />
+    </Link>
+  );
+};
--- src/components/PopupWiner/index.jsx
+++ src/components/PopupWiner/index.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 import './style.css';
 import { PopupWinerExplanation } from '../PopupWinerExplanation';
 import { ContinueArrowHome } from '../ContinueArrowHome';
+import { Link } from 'react-router-dom';
+import { AstronautHome } from '../AstronautHome';
 
 export const PopupWiner = ({ allGamesFinished }) => {
   return (
-    <div className="popup-winer">
+    <div className="popup__winer">
       <PopupWinerExplanation allGamesFinished={allGamesFinished}>
-        <p>
+        <p className="popupWiner__heading">
           <strong>Gratulujeme!</strong>
         </p>
-        <p>Objevil si všechny planetky.</p>
-        <ContinueArrowHome />
+        <p>
+          Skvělá práce! Tvá raketka právě dokončila úžasnou misi a objevila
+          všechny planetky ve vesmíru. Bylo to vzrušující dobrodružství plné
+          barevných planetek a nádherných emocí. Jsi opravdový/a mistr/ka
+          vesmíru a emocií! Tvá odvaha a vytrvalost ti umožnily překonat veškeré
+          překážky a dosáhnout úspěchu. Každá planetka, kterou jsi objevil/a,
+          přináší s sebou jedinečné emoce - radost, lásku, smutek, strach
+          nadšení a další. Pamatuj si, že ve vesmíru i v životě je důležité
+          prožívat a vyjadřovat své emoce. Ať už jsi na planetce nebo na Zemi,
+          buď vždycky srdcem otevřený pro nové dobrodružství, prožívej každý
+          okamžik naplno a neboj se dát najevo své pocity.
+        </p>
+        <p>
+          Děkujeme ti za účast ve hře a doufáme, že se brzy vrátíš na další
+          dobrodružství mezi hvězdami. Vesmír je plný překvapení a čeká na tebe
+        </p>
+        <div className="popupWiner__container">
+          <AstronautHome />
+          <ContinueArrowHome />
+        </div>
       </PopupWinerExplanation>
     </div>
   );
