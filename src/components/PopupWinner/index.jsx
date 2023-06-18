aa72f0804e395c73b42155df9b76a1570576237d
Eliška Korencová <eli.korenc@gmail.com>
2023-06-18 16:56:35 +0200

    Úprava barvy ve winner pop-upu

--- src/components/PopupWinner/index.jsx
+++ src/components/PopupWinner/index.jsx
@@ -12,18 +12,15 @@ export const PopupWinner = ({ dialogWinnerRef }) => {
         <p className="winner__text">
           Skvělá práce! Tvá raketka právě dokončila úžasnou misi a objevila
           všechny planetky ve vesmíru. Bylo to vzrušující dobrodružství plné
-          barevných planetek a nádherných emocí. Jsi opravdový/a mistr/ka
-          vesmíru a emocií! Tvá odvaha a vytrvalost ti umožnily překonat veškeré
-          překážky a dosáhnout úspěchu. Každá planetka, kterou jsi objevil/a,
-          přináší s sebou jedinečné emoce - radost, lásku, smutek, strach
-          nadšení a další. Pamatuj si, že ve vesmíru i v životě je důležité
-          prožívat a vyjadřovat své emoce. Ať už jsi na planetce nebo na Zemi,
-          buď vždycky srdcem otevřený pro nové dobrodružství, prožívej každý
-          okamžik naplno a neboj se dát najevo své pocity.
+          barevných emocí. Tvá odvaha a vytrvalost Ti umožnily překonat veškeré
+          překážky a dosáhnout úspěchu. Pamatuj si, že ve vesmíru i v životě je
+          důležité prožívat a vyjadřovat své emoce. Ať už jsi na planetce nebo
+          na Zemi, buď vždycky srdcem otevřený pro nové dobrodružství, prožívej
+          každý okamžik naplno a neboj se dát najevo své pocity.
         </p>
-        <p>
-          Děkujeme ti za účast ve hře a doufáme, že se brzy vrátíš na další
-          dobrodružství mezi hvězdami. Vesmír je plný překvapení a čeká na tebe
+        <p className="winner__text">
+          Děkujeme Ti za účast ve hře a doufáme, že se brzy vrátíš na další
+          dobrodružství mezi hvězdami. Vesmír je plný překvapení a čeká na Tebe.
         </p>
         <div className="winner__astronaut">
           <AstronautHome />
